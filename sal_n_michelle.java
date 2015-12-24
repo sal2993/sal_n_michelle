ce6139b5264cbe2e6cbf20276568842e6baf301c
sal <sal2993@hotmail.com>
2015-12-23 20:04:47 -0700

    Added a little boost of modivation

--- sal_n_michelle.java
+++ sal_n_michelle.java
@@ -1,5 +1,6 @@
 public class sal_n_michelle {
   public static void main(String[] args) {
     System.out.println("hello world"); //added world 
+    System.out.println("Sal and Michelle rule!"); // added some compliments 
   }
 }
